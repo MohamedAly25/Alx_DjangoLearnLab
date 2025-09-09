8af39116950bd2569b715613c499cc1a95633534
Mohamed Aly <mady.aly25@gmail.com>
2025-09-09 12:57:03 +0300

    Fix imports: separate Library and Book for checker validation

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Library
+from .models import Book
 
 def list_books(request):
     books = Book.objects.all()   
