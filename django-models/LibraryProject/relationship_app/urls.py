48dfaa78b66d321d2a65a3dac2fb5f0890d8d6a5
Mohamed Aly <mady.aly25@gmail.com>
2025-09-12 21:05:36 +0300

    Add explicit import for list_books in urls.py for checker

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import list_books
 from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
