6246630b0375c39e9d830bf8b8536a2cbd1dfee1
Mohamed Aly <mady.aly25@gmail.com>
2025-09-12 22:31:33 +0300

    fix(admin): register CustomUser in bookshelf admin; add bookshelf to INSTALLED_APPS; shim users_app.admin

--- advanced_features_and_security/LibraryProject/users_app/admin.py
+++ advanced_features_and_security/LibraryProject/users_app/admin.py
@@ -1,28 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.utils.translation import gettext_lazy as _
-from bookshelf.models import CustomUser
+"""
+Shim admin module: actual registration lives in `bookshelf.admin`.
+Importing `bookshelf.admin` ensures the admin is registered when Django loads
+this app's admin module.
+"""
 
-
-class CustomUserAdmin(UserAdmin):
-    model = CustomUser
-    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'date_of_birth')
-    search_fields = ('username', 'email', 'first_name', 'last_name')
-    ordering = ('username',)
-
-    fieldsets = (
-        (None, {'fields': ('username', 'password')}),
-        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'date_of_birth', 'profile_photo')}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
-    )
-
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('username', 'email', 'password1', 'password2', 'date_of_birth', 'profile_photo'),
-        }),
-    )
-
-
-admin.site.register(CustomUser, CustomUserAdmin)
+from bookshelf import admin as _bookshelf_admin  # noqa: F401
